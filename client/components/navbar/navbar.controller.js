e5f394b0d95c3f16e410e152cd392542e061affb
Jim OBrien <jimobrien930@gmail.com>
2014-10-18 10:48:38 -0700

    (cleanup)

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -3,8 +3,8 @@
 angular.module('productsystemApp')
   .controller('NavbarCtrl', function ($scope, $location, Auth) {
     $scope.menu = [{
-      'title': 'Home',
-      'link': '/'
+      'title': 'Products',
+      'link': '/products'
     }];
 
     $scope.isCollapsed = true;
